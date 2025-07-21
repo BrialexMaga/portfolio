b04713c119d65c895b02e448ff0fdb4f57fb6d38
Brian Magaña <111398568+BrialexMaga@users.noreply.github.com>
2025-07-21 00:16:50 -0600

    Add project items to project section

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
+def load_json(file_name: str) -> list[dict]:
+    route = "data/" + file_name + ".json"
+
+    with open(route) as f:
+        return json.load(f)
+    
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    projects = load_json("projects")
+
+    return render_template("index.html", projects=projects)
 
 if __name__=="__main__":
     app.run(debug=True)
